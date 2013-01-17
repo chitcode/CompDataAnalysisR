528364c8b02ef8f3091145aa459bf9b89c02d8eb
chitra <chitra@chitra-machine.(none)>
2013-01-17 07:16:36 +0530

    initial code

---
+++ page_253.R
@@ -0,0 +1,33 @@
+# Computational Methods of data analysis
+# Source : page 253
+
+
+
+########################### SOURCE ###################
+# clear all; close all; % clear all variables and figures
+
+# L=20; % define the computational domain [-L/2,L/2]
+# n=128; % define the number of Fourier modes 2^n
+
+# x2=linspace(-L/2,L/2,n+1); % define the domain discretization
+# x=x2(1:n); % consider only the first n points: periodicity
+
+# u=exp(-x.*x); % function to take a derivative of
+# ut=fft(u); % FFT the function
+# utshift=fftshift(ut); % shift FFT
+# figure(1), plot(x,u) % plot initial gaussian
+# figure(2), plot(abs(ut)) % plot unshifted transform
+# figure(3), plot(abs(utshift)) % plot shifted transform
+###########################
+
+L <- 20 # define the computational domain [-L/2, L/2]
+n <- 128 #define the number of Fourier modes 2^n
+
+x2 <- seq(from=-L/2, to = L/2, length.out = n+1) # define the domain discretization
+x <- x2[1:n] # consider only the first n points: periodicity
+
+u <- exp(-x^2) # function to take a derivative of
+ut <- fft(u) # FFT the function
+
+plot(x,u,type='l')
+plot(x,Re(ut),type='l')
